cc11ea3a0588278b216b90a38e5de41d8dfe9c11
Precious Mozia <chibundumozia@gmail.com>
2025-04-03 00:38:12 +0200

    backend

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-uvicorn app.main:app --host=0.0.0.0 --port=$PORT
+
+# Change directory to backend (if necessary)
+cd "$(dirname "$0")/backend"
+
+# Start the FastAPI app with uvicorn on the specified host and port
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
